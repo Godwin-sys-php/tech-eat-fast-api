645a8529dfb5aaaf38f189ae48a3045c2d481c91
Godwin Burume <godwinnyembo@gmail.com>
2021-08-26 16:31:42 +0100

    Ajuout de la création des factures

--- Middlewares/Specials/consulat.js
+++ Middlewares/Specials/consulat.js
@@ -68,6 +68,7 @@ module.exports = async (req, res) => {
     return res.status(200).json({ update: true });
 
   } catch (error) {
+    console.log(error);
     return res.status(500).json({ error: true });
   }
 }
