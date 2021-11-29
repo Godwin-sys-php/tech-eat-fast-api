e935c5ddd7007cb7d5cf52860af321682605e1d8
Godwin Burume <godwinnyembo@gmail.com>
2021-11-29 02:11:59 +0100

    Ajout des notifications

--- Helpers/newCommandNotification.js
+++ Helpers/newCommandNotification.js
@@ -6,25 +6,28 @@ module.exports = async (idRestaurant) => {
     const restaurantUsers = await Restaurants.customQuery("SELECT * FROM usersRestaurant WHERE idRestaurant = ?", [idRestaurant]);
 
     for (let index in restaurantUsers) {
-        const a = await fetch('https://fcm.googleapis.com/fcm/send', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: `key=AAAA1Wlnc64:APA91bF52eGpUQGAQBpsAOIlUvxnvmh00rnhXMN-YoZefUR7_jTIFk_Y3r3ODzom_WnFWNpnc2jIoxOdmK9asNNcMtsRBeTgtdxBcjqSLgwJNks3afmTDadyGaQXRMmNESaElWLFH12P`,
-            },
-            body: JSON.stringify({
-                to: restaurantUsers[index].notificationToken,
-                priority: 'high',
-                data: {
-                    token: restaurantUsers[index].notificationToken,
-                    experienceId: '@nodewin/tef-partners',
-                    title: "Nouvelle commande " + restaurantInfo.name,
-                    message: 'Vous avez une nouvelle commande venez la voir',
-                    channelId: "default",
-                    priority: "max",
+        if (restaurantUsers[index].notificationToken !== null) {
+            console.log(restaurantUsers[index].notificationToken);
+            const a = await fetch('https://fcm.googleapis.com/fcm/send', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `key=AAAA1Wlnc64:APA91bF52eGpUQGAQBpsAOIlUvxnvmh00rnhXMN-YoZefUR7_jTIFk_Y3r3ODzom_WnFWNpnc2jIoxOdmK9asNNcMtsRBeTgtdxBcjqSLgwJNks3afmTDadyGaQXRMmNESaElWLFH12P`,
                 },
-            }),
-        });
-        console.log(await a.json());
+                body: JSON.stringify({
+                    to: restaurantUsers[index].notificationToken,
+                    priority: 'high',
+                    data: {
+                        token: restaurantUsers[index].notificationToken,
+                        experienceId: '@nodewin/tef-partners',
+                        title: "Nouvelle commande " + restaurantInfo.name,
+                        message: 'Vous avez une nouvelle commande venez la voir',
+                        channelId: "default",
+                        priority: "max",
+                    },
+                }),
+            });
+            console.log(await a.json());
+        }
     }
 }
