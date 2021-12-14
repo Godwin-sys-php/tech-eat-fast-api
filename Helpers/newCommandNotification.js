eca384ddded2557168d55f0d50dc229179765e16
Godwin Burume <godwinnyembo@gmail.com>
2021-12-14 01:18:35 +0100

    Modification code notification

--- Helpers/newCommandNotification.js
+++ Helpers/newCommandNotification.js
@@ -2,29 +2,34 @@ const Restaurants = require("../Models/Restaurants");
 const fetch = require("node-fetch");
 
 module.exports = async (idRestaurant) => {
-    const restaurantInfo = await Restaurants.findOne({ idRestaurant: idRestaurant });
-    const restaurantUsers = await Restaurants.customQuery("SELECT * FROM usersRestaurant WHERE idRestaurant = ?", [idRestaurant]);
+  const restaurantInfo = await Restaurants.findOne({
+    idRestaurant: idRestaurant,
+  });
+  const restaurantUsers = await Restaurants.customQuery(
+    "SELECT * FROM usersRestaurant WHERE idRestaurant = ?",
+    [idRestaurant]
+  );
 
-    for (let index in restaurantUsers) {
-        if (restaurantUsers[index].notificationToken !== null) {
-            console.log(restaurantUsers[index].notificationToken);
-            const a = await fetch('https://fcm.googleapis.com/fcm/send', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    Authorization: `key=AAAA1Wlnc64:APA91bF52eGpUQGAQBpsAOIlUvxnvmh00rnhXMN-YoZefUR7_jTIFk_Y3r3ODzom_WnFWNpnc2jIoxOdmK9asNNcMtsRBeTgtdxBcjqSLgwJNks3afmTDadyGaQXRMmNESaElWLFH12P`,
-                },
-                body: JSON.stringify({
-                    to: restaurantUsers[index].notificationToken,
-                    priority: 'high',
-                    data: {
-                        experienceId: '@nodewin/tefp',
-                        title: "Nouvelle commande " + restaurantInfo.name,
-                        message: 'Vous avez une nouvelle commande venez la voir',
-                    },
-                }),
-            });
-            console.log(await a.json());
-        }
+  for (let index in restaurantUsers) {
+    if (restaurantUsers[index].notificationToken !== null) {
+      console.log(restaurantUsers[index].notificationToken);
+      const message = {
+        to: restaurantUsers[index].notificationToken,
+        sound: 'default',
+        title: 'Nouvelle commande',
+        body: 'Vous avez une nouvelle commande, venez la voir',
+        channelId:	"commandReceiver",
+      };
+    
+      await fetch('https://api.expo.dev/v2/push/send', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Accept-encoding': 'gzip, deflate',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(message),
+      });
     }
-}
+  }
+};
