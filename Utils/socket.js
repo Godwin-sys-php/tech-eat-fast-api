3db709dfdd8cc08021337acccfa04e802658be80
Godwin Burume <godwinnyembo@gmail.com>
2022-07-12 13:24:20 +0100

    correction du websocket

--- Utils/socket.js
+++ Utils/socket.js
@@ -1,21 +1,23 @@
-const socketIo = require('socket.io');
+const socketIo = require("socket.io");
 
 class SocketService {
-   constructor(server) {
-     this.io = socketIo(server);
-     this.io.on('connection', socket => {
-       console.log('user connected')
-   });
- } 
+  constructor(server) {
+    this.io = socketIo(server, {
+      cors: {
+        origin: "*",
+      },
+    });
+    this.io.on("connection", (socket) => {
+      console.log("user connected");
+    });
+  }
 
   emiter(event, body) {
-    if(body)
-      this.io.emit(event, body);
+    if (body) this.io.emit(event, body);
   }
 
   broadcastEmiter(event, body) {
-    if(body)
-      this.io.emit(event, body);
+    if (body) this.io.emit(event, body);
   }
 }
 
